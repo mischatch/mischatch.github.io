b7c3ca82a810892c3ec466b08f1b1caf753d2069
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-05-16 10:33:45 -0400

    disabled dispalay script for photo

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 const hello = document.getElementById('hello');
 const img = document.getElementById('photo');
-img.style.display = 'none';
+// img.style.display = 'none';
 
 
 hello.addEventListener('mouseover', (e) => {
-  img.style.removeProperty('display');
+  // img.style.removeProperty('display');
   e.target.style.color = 'white';
 });
 
 hello.addEventListener('mouseout', (e) => {
-  img.style.display = 'none';
+  // img.style.display = 'none';
   e.target.style.color = 'black';
 });
 
