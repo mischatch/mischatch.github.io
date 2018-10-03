8f85f448aeea53802e52068af3a5ba6600f9e0b0
mischatch <mischa.tch@gmail.com>
2018-10-03 13:09:43 -0400

    some changes

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,16 @@ const hello = document.getElementById('hello');
 const img = document.getElementById('photo');
 // img.style.display = 'none';
 
-
-hello.addEventListener('mouseover', (e) => {
-  // img.style.removeProperty('display');
-  e.target.style.color = 'white';
-});
-
-hello.addEventListener('mouseout', (e) => {
-  // img.style.display = 'none';
-  e.target.style.color = 'black';
-});
+//
+// hello.addEventListener('mouseover', (e) => {
+//   // img.style.removeProperty('display');
+//   e.target.style.color = 'white';
+// });
+//
+// hello.addEventListener('mouseout', (e) => {
+//   // img.style.display = 'none';
+//   e.target.style.color = 'black';
+// });
 
 if (document.location.href === "http://www.mischatch.me/index.html") {
   document.location.href = "http://www.mischatch.me/";
