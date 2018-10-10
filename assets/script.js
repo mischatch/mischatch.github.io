9e56a381837e1b59a98861af9de927132a7a1591
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-10-10 11:15:17 -0400

    added gtag event trackers and jquery

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,41 @@
-const hello = document.getElementById('hello');
-const img = document.getElementById('photo');
-// img.style.display = 'none';
+$(document).ready(function(){
+    console.log('Doc ready!')
 
-//
-// hello.addEventListener('mouseover', (e) => {
-//   // img.style.removeProperty('display');
-//   e.target.style.color = 'white';
-// });
-//
-// hello.addEventListener('mouseout', (e) => {
-//   // img.style.display = 'none';
-//   e.target.style.color = 'black';
-// });
+    const hello = document.getElementById('hello');
+    const img = document.getElementById('photo');
+    // img.style.display = 'none';
 
-if (document.location.href === "http://www.mischatch.me/index.html") {
-  document.location.href = "http://www.mischatch.me/";
-}
+    //
+    // hello.addEventListener('mouseover', (e) => {
+    //   // img.style.removeProperty('display');
+    //   e.target.style.color = 'white';
+    // });
+    //
+    // hello.addEventListener('mouseout', (e) => {
+    //   // img.style.display = 'none';
+    //   e.target.style.color = 'black';
+    // });
 
-// var width = img.outerHeight();
-// img.width(width);
-$(document).resize(function(){
-    var width = img.outerHeight();
-    img.width(width);
+    if (document.location.href === "http://www.mischatch.me/index.html") {
+        document.location.href = "http://www.mischatch.me/";
+    }
+
+    // var width = img.outerHeight();
+    // img.width(width);
+    $(document).resize(function(){
+        var width = img.outerHeight();
+        img.width(width);
+    })
+
+    var link = $('.links span a');
+    var worksLink = $('.works-link a');
+    link.on('click', function(e){
+        var name = $(this).html();
+        gtag('event', 'click', { 'event_category': 'link', 'event_label': name });
+    })
+
+    worksLink.on('click', function(e){
+        gtag('event', 'click', { 'event_category': 'link', 'event_label': 'go to works page' });
+    })
+
 })
