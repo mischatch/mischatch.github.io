e6679735d52f34cf68dca770fe60980c498eafc3
mischatch <mischa.tch@gmail.com>
2021-01-23 17:22:53 -0500

    bg change

--- assets/script.js
+++ assets/script.js
@@ -24,4 +24,71 @@ $(document).ready(function(){
         gtag('event', 'click', { 'event_category': 'link', 'event_label': 'go to works page' });
     });
 
+    // color selection
+
+    function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays*24*60*60*1000));
+      var expires = "expires="+ d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+
+    $('.info-box .info-button').click(function(e){
+      $('.info-box .box').toggleClass('open')
+    })
+
+    $(document).mouseup(function(e){
+          var container = $('.info-box .box');
+          if (!container.is(e.target) && container.has(e.target).length === 0){
+              container.removeClass('open');
+          }
+      });
+
+    var bg = getCookie('background');
+
+    if(bg !== ''){
+      // $('input#background').val(bg);
+      $('body').css("background-color", bg);
+    }
+    //
+    // $('input#background').change(function(e){
+    //   $('body').css("background-color", this.value);
+    //
+    //   console.log('new background', this.value)
+    //   setCookie('background', this.value, 30);
+    // });
+
+    // var colorPicker = new iro.ColorPicker('#picker');
+
+    var colorPicker = new iro.ColorPicker("#picker", {
+      // Set the size of the color picker
+      width: 150,
+      // Set the initial color to pure red
+      color: bg !== '' ? bg : '#55ffbd'
+    });
+
+    colorPicker.on(['color:change'], function(color) {
+        $('body').css("background-color", color.hexString);
+
+        console.log('new background', color.hexString)
+        setCookie('background', color.hexString, 30);
+    });
+
+
 })
